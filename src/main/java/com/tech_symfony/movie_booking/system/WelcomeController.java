cbd7d606106967b2914962c6acfd57969820aed5
Nguyen Thanh Duy <thanhduy191103@gmail.com>
2024-05-13 14:32:43 +0700

    chore: add cache, default homepage, crash page

---
+++ src/main/java/com/tech_symfony/movie_booking/system/WelcomeController.java
@@ -0,0 +1,15 @@
+
+package com.tech_symfony.movie_booking.system;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+class WelcomeController {
+
+	@GetMapping("/")
+	public String welcome() {
+		return "welcome";
+	}
+
+}
