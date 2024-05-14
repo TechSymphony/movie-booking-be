75c762c612641c30f84c2d144208abac9f776a26
Tiền Minh Vy <tienminhvydev@gmail.com>
2024-05-14 09:32:05 +0700

    refactor: adding Makefile to simplify dockerization process

--- src/main/java/com/tech_symfony/movie_booking/employee/EmployeeController.java
+++ src/main/java/com/tech_symfony/movie_booking/employee/EmployeeController.java
@@ -10,9 +10,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.stream.Collectors;
+
 @RestController
 class EmployeeController {
 
@@ -20,6 +22,11 @@ class EmployeeController {
 
     EmployeeController(EmployeeRepository repository) {
         this.repository = repository;
+    }
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World!";
     }
 
     // Aggregate root
