e39389d5ab8791e5e9660bff4d5925a77843c3d9
Nguyen Thanh Duy <62139508+1119-DuyNguyen@users.noreply.github.com>
2024-05-15 14:24:32 +0700

    fix- bug data lombok Person.java

--- src/main/java/com/tech_symfony/movie_booking/model/Person.java
+++ src/main/java/com/tech_symfony/movie_booking/model/Person.java
@@ -3,7 +3,6 @@ package com.tech_symfony.movie_booking.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Data;
 
 /**
  * Simple JavaBean domain object representing an person.
@@ -11,9 +10,9 @@ import lombok.Data;
  *
  */
 @MappedSuperclass
-@Data
 public class Person extends BaseEntity {
 
+	
 	@Column(name = "first_name")
 	@NotBlank
 	private String firstName;
@@ -21,5 +20,22 @@ public class Person extends BaseEntity {
 	@Column(name = "last_name")
 	@NotBlank
 	private String lastName;
+
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return this.lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
 
 }
