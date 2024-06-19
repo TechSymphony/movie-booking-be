5e06455ed4cf28f53e1c996a653b606449ced778
ngocson741852963 <117715756+ngocson741852963@users.noreply.github.com>
2024-06-19 16:45:38 +0700

    feat(role): success rbac

--- src/main/java/com/tech_symfony/movie_booking/api/role/permission/PermissionService.java
+++ src/main/java/com/tech_symfony/movie_booking/api/role/permission/PermissionService.java
@@ -1,13 +1,29 @@
 package com.tech_symfony.movie_booking.api.role.permission;
 
+import com.tech_symfony.movie_booking.api.role.RoleRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Map;
+
 @Service
+@RequiredArgsConstructor
 public class PermissionService {
 
+	private final RoleRepository roleRepository;
+
 	public boolean hasPermission(Authentication authentication, String permission) {
-		return authentication.getAuthorities().stream()
-			.anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals(permission));
+		if (authentication.getPrincipal() instanceof Jwt) {
+			Jwt jwt = (Jwt) authentication.getPrincipal();
+			String email = jwt.getClaimAsString("sub");
+			List<String> userPermissions = roleRepository.getPermissionByUser(email);
+			return userPermissions.contains(permission);
+		}
+		return false;
 	}
 }
