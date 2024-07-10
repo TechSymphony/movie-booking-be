e9ee124b4ece25ec260956b44824feb40c36de32
1119-DuyNguyen <thanhduy191103@gmail.com>
2024-07-10 20:45:33 +0700

    fix: application not loaded integration testing

---
+++ src/test/java/com/tech_symfony/movie_booking/MovieBookingApplicationIT.java
@@ -0,0 +1,15 @@
+package com.tech_symfony.movie_booking;
+
+import com.tech_symfony.movie_booking.model.BaseIntegrationTest;
+import org.junit.jupiter.api.Test;
+
+
+class MovieBookingApplicationIT extends BaseIntegrationTest {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+
+}
---
+++ src/test/java/com/tech_symfony/movie_booking/api/RestdocsExampleIT.java
@@ -0,0 +1,51 @@
+package com.tech_symfony.movie_booking.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tech_symfony.movie_booking.MovieBookingApplication;
+import com.tech_symfony.movie_booking.api.RootController;
+import com.tech_symfony.movie_booking.model.BaseIntegrationTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.resourceDetails;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.linkWithRel;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.links;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.subsectionWithPath;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.util.StringUtils.collectionToDelimitedString;
+import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.document;
+
+public class RestdocsExampleIT extends BaseIntegrationTest {
+	@Autowired
+	private ObjectMapper objectMapper;
+
+
+	@Test
+	public void indexExample() throws Exception {
+		this.mockMvc.perform(get("/"))
+			.andExpect(status().isOk())
+			.andDo(document("index-example",
+				resourceDetails()
+					.description("Get all base links."),
+				links(linkWithRel("login").description("The login")
+
+					, linkWithRel("movies").description("The movies resource")
+
+					, linkWithRel("cinemas").description("The cinemas resource")
+
+					, linkWithRel("showtimes").description("The showtimes resource")),
+
+				responseFields(subsectionWithPath("_links").description("Links to other resources")),
+				responseHeaders(headerWithName("Content-Type").description("The Content-Type of the payload, e.g. `application/hal+json`"))));
+	}
+
+
+}
