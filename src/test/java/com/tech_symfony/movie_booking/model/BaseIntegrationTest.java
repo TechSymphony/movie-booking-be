d717df88fa8faec55e4efc816aa3b24c4c48f79b
1119-DuyNguyen <thanhduy191103@gmail.com>
2024-07-10 20:45:33 +0700

    fix: application not loaded integration testing

--- src/test/java/com/tech_symfony/movie_booking/model/BaseIntegrationTest.java
+++ src/test/java/com/tech_symfony/movie_booking/model/BaseIntegrationTest.java
@@ -1,16 +1,25 @@
 package com.tech_symfony.movie_booking.model;
 
+import com.tech_symfony.movie_booking.system.config.ESTestConfiguration;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.cli.CliDocumentation;
 import org.springframework.restdocs.http.HttpDocumentation;
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
 import org.springframework.restdocs.mockmvc.UriConfigurer;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -18,24 +27,14 @@ import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.header;
 
 @AutoConfigureRestDocs
 @ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
 @TestPropertySource(properties = "spring.jackson.serialization.indent_output=true")
+@SpringBootTest
+@Import(ESTestConfiguration.class)
 public abstract class BaseIntegrationTest {
 	protected MockMvc mockMvc;
-
-
-	//
-//
-	protected static MockHttpServletRequestBuilder getAuthenticateHeader(MockHttpServletRequestBuilder request) {
-		return request;
-
-//		request.header("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c=");
-//
-//		return request;
-	}
 
 	@BeforeEach
 	public void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
@@ -55,5 +54,6 @@ public abstract class BaseIntegrationTest {
 			.alwaysDo(document("{class-name}/{method-name}", preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint())))
 			.build();
 	}
+
 
 }
