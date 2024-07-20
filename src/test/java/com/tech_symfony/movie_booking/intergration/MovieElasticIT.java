6b7ae411b7a7394034e7640a3a755820b93c58f0
Tiền Minh Vy <tienminhvydev@gmail.com>
2024-07-20 21:26:17 +0700

    test: added integration test for Elasticsearch

--- src/test/java/com/tech_symfony/movie_booking/intergration/MovieElasticIT.java
+++ src/test/java/com/tech_symfony/movie_booking/intergration/MovieElasticIT.java
@@ -1,11 +1,18 @@
 package com.tech_symfony.movie_booking.intergration;
 
 import com.tech_symfony.movie_booking.api.movie.elastic.MovieElasticController;
+import com.tech_symfony.movie_booking.api.movie.elastic.MovieSearch;
 import com.tech_symfony.movie_booking.model.BaseIntegrationTest;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 
+import java.sql.Date;
 import java.util.Optional;
 
 import static org.slf4j.MDC.get;
@@ -15,6 +22,32 @@ import static org.springframework.web.servlet.mvc.method.annotation.MvcUriCompon
 
 public class MovieElasticIT extends BaseIntegrationTest {
 
+	@Autowired
+	private ElasticsearchOperations operations;
+
+	@BeforeEach
+	public void setup() {
+		MovieSearch movieSearch = new MovieSearch();
+		movieSearch.setId(1);
+		movieSearch.setCode("mv-0002");
+		movieSearch.setName("The Godfather");
+		movieSearch.setSubName("The Godfather");
+		movieSearch.setDirector("Francis Ford Coppola");
+		movieSearch.setCaster("Mario Puzo");
+		movieSearch.setReleaseDate(Date.valueOf("1972-03-24"));
+		movieSearch.setEndDate(Date.valueOf("1972-03-24"));
+		movieSearch.setRunningTime(175);
+		movieSearch.setLanguage("English");
+		movieSearch.setNumberOfRatings(100);
+		movieSearch.setSumOfRatings(1000);
+		movieSearch.setDescription("The aging patriarch of an organized crime dynasty in postwar New York City transfers control of his clandestine empire to his reluctant youngest son.");
+		movieSearch.setPoster("https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg");
+		movieSearch.setHorizontalPoster("https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg");
+		movieSearch.setTrailer("https://www.youtube.com/watch?v=sY1S34973zA");
+		movieSearch.setRated(5);
+		movieSearch.setSlug("the-godfather");
+		operations.save(movieSearch);
+	}
 	@Test
 	public void getMovieByName() throws Exception {
 		this.mockMvc.perform(RestDocumentationRequestBuilders.get(fromMethodCall(on(MovieElasticController.class).search(Pageable.unpaged(), Optional.of("Past"))).build().toUriString()))
